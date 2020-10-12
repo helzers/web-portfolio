533388e302ac0029e112e85272a8eeb07a47fbe1
Helen <helmurphy6@gmail.com>
2020-10-12 18:23:07 +0100

    Changing router to hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TopBar from "./Components/TopBar";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './Pages/Home';
 import WorkExperience from './Pages/Experience';
 import About from './Pages/About';
--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -5,9 +5,6 @@ import '../CSS/TopBar.css';
 function TopBar() {
     return (
         <div className="navbar">
-            {/* <Navbar fixed="top" bg="light" variant="light">
-                <Navbar.Brand href="/">Helen Murphy</Navbar.Brand>
-            </Navbar> */}
             <nav className="nav-items">
                 <NavLink className="nav-bar-home" activeClassName="active" exact to="/">Helen Murphy</NavLink>
                 <NavLink className="nav-bar-about" activeClassName="active" to="/about">About</NavLink>
