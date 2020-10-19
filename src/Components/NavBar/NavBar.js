a8d6e049cf788b6bf5385daf24d1c4aec46af981
Helen <helmurphy6@gmail.com>
2020-10-19 17:01:00 +0100

    Nav bar Styling

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -4,15 +4,17 @@ import './NavBar.scss';
 
 function NavBar() {
     return (
-        <nav className="nav-items">
-            <NavLink className="nav-bar-home" activeClassName="active" exact to="/">Helen Murphy</NavLink>
-            <NavLink className="nav-bar-experience" activeClassName="active" to="/experience">Experience</NavLink>
-            <NavLink className="nav-bar-projects" activeClassName="active" to="/projects">Projects</NavLink>
+        <div className="navbar">
+            <nav className="nav-items">
+                <NavLink className="nav-bar-home" activeClassName="active" exact to="/">Helen Murphy</NavLink>
+                <NavLink className="nav-bar-experience" activeClassName="active" to="/experience">Experience</NavLink>
+                <NavLink className="nav-bar-projects" activeClassName="active" to="/projects">Projects</NavLink>
+            </nav>
             <div className="img-links">
-                <a className="nav-bar-gh" href="https://github.com/helzers"><img src="/Images/Nav/GH.png" alt="GitHub"/></a>
-                <a className="nav-bar-li" href="https://www.linkedin.com/in/helen-murphy-8895b6107/"><img src="/Images/GH.png" alt="LinkedIn"/></a>
+                <a className="nav-bar-gh" href="https://github.com/helzers"><img src={process.env.PUBLIC_URL + '/Images/Nav/GH.png'} alt="GitHub"/></a>
+                <a className="nav-bar-li" href="https://www.linkedin.com/in/helen-murphy-8895b6107/"><img src={process.env.PUBLIC_URL + '/Images/Nav/linkedin-512.png'} alt="LinkedIn"/></a>
             </div>
-        </nav>
+        </div>
     )
 }
 
