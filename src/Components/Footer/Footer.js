6673b5a36b1d6f4956101de896aed480b2e4cb87
Helen <helmurphy6@gmail.com>
2020-11-11 23:00:57 +0000

    trying relative paths for footer link issue

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,15 +5,15 @@ function Footer() {
     return (
         <div className="footer">
             <div className="website-nav">
-                <a exact href="https://helzers.github.io/web-portfolio/#/">Home</a>
-                <a exact href="https://helzers.github.io/web-portfolio/#/experience">Work Experience</a>
-                <a exact href="https://helzers.github.io/web-portfolio/#/projects">Projects</a>
+                <a exact href="./#/">Home</a>
+                <a exact href="./#/experience">Work Experience</a>
+                <a exact href="./#/projects">Projects</a>
             </div>
             <div className="footer-logo">
-                <a className="footer-logo-link" exact href="https://helzers.github.io/web-portfolio/#/"><img src={process.env.PUBLIC_URL + "/Images/Nav/HMSemiBold.png"} alt="Helen Logo"/></a>
+                <a className="footer-logo-link" exact href="./#/"><img src={process.env.PUBLIC_URL + "/Images/Nav/HMSemiBold.png"} alt="Helen Logo"/></a>
             </div>
             <div className="contact">
-                <a exact href="https://helzers.github.io/web-portfolio/#/contactme">Contact Me</a>
+                <a exact href="./web-portfolio/#/contactme">Contact Me</a>
                 <a exact href={process.env.PUBLIC_URL + "/Helen_Murphy_CV2020.docx"} download>Download my CV</a>
             </div>
         </div>
