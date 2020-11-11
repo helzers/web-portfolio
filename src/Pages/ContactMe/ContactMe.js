fd565541e4e7b8c3c5c96a7116fb7153bda9c19e
Helen <helmurphy6@gmail.com>
2020-11-11 23:29:21 +0000

    adding skeleton of contact me form

--- src/Pages/ContactMe/ContactMe.js
+++ src/Pages/ContactMe/ContactMe.js
@@ -11,28 +11,24 @@ function ContactMe() {
         <div className="contactMe">
             <h1>Contact Me</h1>
             <div className="contactDetails">
-                <p id="contactInfo">Contact me using this form, or you can reach me via email or LinkedIn</p>
+                <p id="contactInfo">Feel free to contact me at any time regarding any job opportunities. You can contact me using this form, or you can reach me via email or LinkedIn. Looking forward to hearing from you</p>
                 <h4>Email:</h4>
                 <p id="contactEmail">helmurphy6@gmail.com</p>
                 <a href="https://www.linkedin.com/in/helen-murphy-8895b6107/"><img src={process.env.PUBLIC_URL + "/Images/Linkedin-Logo.png"} alt="LinkedIn" /></a>
             </div>
-            {/* <div className="contactForm">
-                <form>
-                    <input type="text" id="name" name="name" />
-                    <label for="name">Your Name</label>
+            <div className="contactMeForm">
+                <form className="contact-form">
+                    <input type="text" id="name" name="name" placeholder="Your Name"/>
                     
-                    <input type="text" id="email" name="email" />
-                    <label for="email">Your Email</label>
+                    <input type="text" id="email" name="email" placeholder="Your Email"/>
 
-                    <input type="text" id="subject" name="subject" />
-                    <label for="subject">Subject</label>
+                    <input type="text" id="subject" name="subject" placeholder="Subject"/>
 
-                    <textarea id="message" name="message" />
-                    <label for="message">Your Message</label>
+                    <textarea id="message" name="message" placeholder="Your Message"/>
 
                     <button type="button">Submit</button>
                 </form>
-            </div> */}
+            </div>
         </div>
     )
 }
