45c977195ef58fccb2375791f4de1cb32a5728fc
Helen <helmurphy6@gmail.com>
2020-11-13 15:53:11 +0000

    adding handlers for contact me form

--- src/Pages/ContactMe/ContactMe.js
+++ src/Pages/ContactMe/ContactMe.js
@@ -1,11 +1,71 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './ContactMe.scss'
 
 function ContactMe() {
     
+    // Use States 
+    var [name, setName] = useState("");
+    var [email, setEmail] = useState("");
+    var [subject, setSubject] = useState("");
+    var [message, setMessage] = useState("");
+
+    var [contactInfo, setContactInfo] = useState({name: '', email: '', subject: '', message: ''})
+    
+    
+    //Functions
+    var onNameChange = (e) => {
+        setName(e.target.value);
+    }
+    var onEmailChange = (e) => {
+        setEmail(e.target.value);
+    }
+    var onSubjectChange = (e) => {
+        setSubject(e.target.value);
+    }
+    var onMessageChange = (e) => {
+        setMessage(e.target.value);
+    }
+
+    var getContactInfo = e => {
+        e.preventDefault();
+
+        setContactInfo({name: name});
+        setContactInfo({email: email});
+        setContactInfo({subject: subject});
+        setContactInfo({message: message});
+
+        submitEmail();
+
+        
+    }
+
+    var submitEmail = () => {
+        // axios({
+        //   method: "POST", 
+        //   url:"/send", 
+        //   data:  contactInfo
+        // }).then((response)=>{
+        //   if (response.data.status === 'success'){
+        //       alert("Message Sent."); 
+        //       resetForm()
+        //   }else if(response.data.status === 'fail'){
+        //       alert("Message failed to send.")
+        //   }
+        // })
+    }
+
+    var resetForm = () => {
+        setName("");
+        setEmail("");
+        setSubject("");
+        setMessage("");
+    }
+
+    //Use Effects
     useEffect(() => {
         window.scrollTo(0,0);
     }, []);
+    
     
     return (
         <div className="contactMe">
@@ -17,14 +77,14 @@ function ContactMe() {
                 <a href="https://www.linkedin.com/in/helen-murphy-8895b6107/"><img src={process.env.PUBLIC_URL + "/Images/Linkedin-Logo.png"} alt="LinkedIn" /></a>
             </div>
             <div className="contactMeForm">
-                <form className="contact-form">
-                    <input type="text" id="name" name="name" placeholder="Your Name"/>
+                <form className="contact-form" onSubmit={getContactInfo}>
+                    <input type="text" id="name" name="name" placeholder="Your Name" onChange={onNameChange}/>
                     
-                    <input type="text" id="email" name="email" placeholder="Your Email"/>
+                    <input type="text" id="email" name="email" placeholder="Your Email" onChange={onEmailChange}/>
 
-                    <input type="text" id="subject" name="subject" placeholder="Subject"/>
+                    <input type="text" id="subject" name="subject" placeholder="Subject" onChange={onSubjectChange}/>
 
-                    <textarea id="message" name="message" placeholder="Your Message"/>
+                    <textarea id="message" name="message" placeholder="Your Message" onChange={onMessageChange}/>
 
                     <button type="button">Submit</button>
                 </form>
